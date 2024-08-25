fc3b2f73bf8aefbaff82c89a292f40c66b23de21
weiyi.wang <weiyi.wang@iherb.com>
2024-08-25 17:48:44 +0800

    udpate script

--- docker-amd64.sh
+++ docker-amd64.sh
@@ -2,18 +2,7 @@ export VERSION=${VERSION:-$(git describe --tags --first-parent --abbrev=7 --long
 export GOFLAGS="'-ldflags=-w -s \"-X=github.com/ollama/ollama/version.Version=$VERSION\" \"-X=github.com/ollama/ollama/server.mode=release\"'"
 
 DOCKER_ORG=${DOCKER_ORG:-"siegrainwong"}
-RELEASE_IMAGE_REPO=${RELEASE_IMAGE_REPO:-"${DOCKER_ORG}/release"}
-FINAL_IMAGE_REPO=${FINAL_IMAGE_REPO:-"${DOCKER_ORG}/ollama"}
-
-# docker build \
-#     --load \
-#     --platform=linux/amd64 \
-#     --build-arg=VERSION \
-#     --build-arg=GOFLAGS \
-#     --target runtime-rocm \
-#     -f Dockerfile \
-#     -t ${RELEASE_IMAGE_REPO}:$VERSION-rocm \
-    .
+RELEASE_IMAGE_REPO=${RELEASE_IMAGE_REPO:-"${DOCKER_ORG}/ollama"}
 
 docker build \
     --load \
